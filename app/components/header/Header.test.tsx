90d070a151e93c6a9410057d51ff9e995fef26e9
egomezsalasva <egomezsalasva@gmail.com>
2025-05-19 01:37:44 +0200

    add header

--- app/components/header/Header.test.tsx
+++ app/components/header/Header.test.tsx
@@ -1,9 +1,33 @@
-import { render } from "@testing-library/react";
-import { describe, it } from "vitest";
+import { render, screen, within } from "@testing-library/react";
+import { beforeEach, describe, expect, it } from "vitest";
 import Header from "./Header";
 
 describe("Header", () => {
-  it("renders without crashing", () => {
+  beforeEach(() => {
     render(<Header />);
+  });
+  it("should have a svg logo", () => {
+    const logo = screen.getByTestId("logo");
+    expect(logo).toBeInTheDocument();
+    const svg = logo.querySelector("svg");
+    expect(svg).toBeInTheDocument();
+  });
+  it("should have a list of links", () => {
+    const nav = screen.getByTestId("nav");
+    const links = within(nav).getAllByRole("link");
+    expect(links).toHaveLength(4);
+    expect(links[0]).toHaveTextContent("LATEST");
+    expect(links[0]).toHaveAttribute("href", "/");
+    expect(links[1]).toHaveTextContent("STYLES");
+    expect(links[1]).toHaveAttribute("href", "/styles");
+    expect(links[2]).toHaveTextContent("EVENTS");
+    expect(links[2]).toHaveAttribute("href", "/events");
+    expect(links[3]).toHaveTextContent("SUPPORT");
+    expect(links[3]).toHaveAttribute("href", "/support-styleista");
+  });
+  it("should have a link to my account", () => {
+    const link = screen.getByText("MY ACCOUNT");
+    expect(link).toBeInTheDocument();
+    expect(link).toHaveAttribute("href", "/my-account");
   });
 });
--- app/components/header/Header.tsx
+++ app/components/header/Header.tsx
@@ -1,9 +1,34 @@
+import Link from "next/link";
+import Logo from "./@svgs/Logo";
 import styles from "./Header.module.css";
 
 const Header = () => {
   return (
-    <header className={styles.header}>
-      <h1>Header</h1>
+    <header className={styles.container}>
+      <div className={styles.headerLeft}>
+        <div className={styles.logoContainer} data-testid="logo">
+          <Logo />
+        </div>
+        <nav className={styles.navContainer} data-testid="nav">
+          <ul>
+            <li>
+              <Link href="/">LATEST</Link>
+            </li>
+            <li>
+              <Link href="/styles">STYLES</Link>
+            </li>
+            <li>
+              <Link href="/events">EVENTS</Link>
+            </li>
+            <li className={styles.supportBtn}>
+              <Link href="/support-styleista">SUPPORT</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+      <div className={styles.headerRight}>
+        <Link href="/my-account">MY ACCOUNT</Link>
+      </div>
     </header>
   );
 };
